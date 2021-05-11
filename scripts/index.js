ff6c0662b529bcb02b831eb5d235e0dcb07ff40f
Nadia <nadezda.mitkaleva@gmail.ru>
2021-05-12 02:19:05 +0500

    Editing tasks

--- scripts/index.js
+++ scripts/index.js
@@ -2,45 +2,132 @@
 
 const countElement = document.querySelector('.footer__count');
 const itemsContainer = document.querySelector('.tasks__list');
-const addInput = document.querySelector('.add__field');
+const addInput = document.querySelector('.add__input');
 const addButton = document.querySelector('.add__button');
-// что, с чем, когда
-// что: вписать количество айтемов
-// что с чем когда
-// что - количесвто айтемов, с чем - с айтемами
-function repaintTasksCount() {
-  const itemsLength = itemsContainer.children.length;
-  countElement.textContent = itemsLength;
+const addForm = document.querySelector('.add__form');
+const templateTask = document.querySelector('#template-task').content.querySelector('.item');
+const footerCount = document.querySelector('.footer__count');
+const clearButton = document.querySelector('.footer__clear-button');
+let editTarget = null;
+
+// редактирование:
+// можно кликнуть на текст
+// текст появится в строке добавления карточки
+// кнопка добавления сменит значек с плюса на галочку
+// редактируемая строка подсветится желтым (ДЗ)
+// при клике на кнопку с галочкой инпут станет пустым, значок сменится на плюс, строка заимеет новое значение и перестанет быть желтой
+
+function clearAll() {
+  const items = document.querySelectorAll('.item');
+
+  items.forEach(item => {
+    item.remove();
+  });
+
+  changeCountOfTasks();
 }
 
-function addTodo() {
-  const value = addInput.value;
-  
-  const newTask = document.createElement('li');
-  newTask.classList.add('tasks__item');
+clearButton.addEventListener('click', clearAll);
 
-  const label = document.createElement('label');
-  label.classList.add('tasks__label', 'field');
+function deleteTask(evt) {
+  evt.target.closest('.item').remove();
+  changeCountOfTasks();
+}
 
-  const input = document.createElement('input');
-  input.classList.add('tasks__input', 'visually-hidden');
-  input.setAttribute('type', 'checkbox');
+document.addEventListener('click', evt => {
+  const itemCheckbox = document.querySelectorAll('.item__indicator');
 
-  const indicator = document.createElement('span');
-  indicator.classList.add('tasks__indicator');
+  if (evt.target.classList.contains('item__trash')) {
+    return deleteTask(evt);
+  }
 
-  const taskText = document.createElement('span');
-  taskText.classList.add('tasks__text');
-  taskText.textContent = value;
+  itemCheckbox.forEach(item => {
+    if (evt.target.classList.contains('item__indicator')) {
+      evt.target.classList.toggle('item__indicator--checked');
+    }
+  })
 
-  const trash = document.createElement('span');
-  trash.classList.add('tasks__trash');
+  if (evt.target.classList.contains('item__text')) {
+    if (editTarget === null) {
+      editTarget = evt.target;
+      addInput.value = evt.target.textContent;
+      addButton.textContent = 'v';
+      evt.target.style.backgroundColor = '#FFFACD';
+      // галочка
+      // раскрасить
+      // занчение в инпут
+    }
+  }
+});
 
-  label.append(input, indicator, taskText, trash);
-  newTask.append(label);
-  itemsContainer.append(newTask);
+function createTodo(data) {
+  const taskItem = templateTask.cloneNode(true);
+  taskItem.querySelector('.item__text').textContent = data;
+
+  return taskItem;
+}
+
+function addTask(evt) {
+  evt.preventDefault();
+  const itemText = document.querySelector('.item__text')
+  let value = addInput.value;
+
+  if (editTarget) {
+    editTarget = null;
+    itemText.textContent = value;
+    addButton.textContent = '+';
+    itemText.style.backgroundColor = '#f7f7f7';
+    // заменить значение
+    // убрать раскраску
+    // заменить обратно на +
+    // занулить editTarget = null;
+  } else {
+    itemsContainer.appendChild(createTodo(value));
+    changeCountOfTasks();
+  }
 
   addInput.value = '';
 }
 
-addButton.addEventListener('click', addTodo);
+addForm.addEventListener('submit', addTask);
+
+// 
+
+const initialCards = [
+  'Buy a new gaming laptop', 
+  'Complete a previous task', 
+  'Create video for YouTube', 
+  'Create a new portfolio site'
+];
+
+function renderTasks(arr) {
+  arr.forEach(item => {
+    itemsContainer.appendChild(createTodo(item));
+  });
+}
+
+renderTasks(initialCards);
+
+//
+function createNewTask(str) {  
+  itemsContainer.appendChild(createTodo(str)); 
+  changeCountOfTasks();
+}
+
+createNewTask('я сделяль');
+
+function changeCountOfTasks() {
+  const itemLenght = itemsContainer.children.length;
+  footerCount.textContent = itemLenght;
+}
+
+
+//
+
+// Бонус:
+// абстрагироваться, и создать универсальную функцию, которая на вход принимает текст, на выход дает карточку(в памяти JS)
+// удалить все начальные карточки из разметки
+// написать код, который на старте приложения добавит какие-то начальные карточки, которые будут описаны в массиве initialCards = ['card1', 'card2, 'card3']
+
+//
+
